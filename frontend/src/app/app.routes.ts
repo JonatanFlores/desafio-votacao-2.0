55ad911f20536c985c0cc57339c450613776009f
Jonatan Flores <jonatafloress@gmail.com>
2024-02-23 17:16:10 -0300

    compose the frontend app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ListAgendaComponent } from './agenda/list-agenda/list-agenda.component';
+import { AddAgendaComponent } from './agenda/add-agenda/add-agenda.component';
+import { AddVoteComponent } from './vote/add-vote/add-vote.component';
+import { ShowAgendaComponent } from './agenda/show-agenda/show-agenda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ListAgendaComponent },
+  { path: 'view/agenda/:agendaId', component: ShowAgendaComponent },
+  { path: 'add/agenda', component: AddAgendaComponent },
+  { path: 'add/vote/:agendaId', component: AddVoteComponent },
+];
